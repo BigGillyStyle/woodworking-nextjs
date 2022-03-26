e74f9bf5ddfb9835bb6f9badf7f48b6140b748a7
Andy Pickler <andy.pickler@gmail.com>
2022-03-25 19:40:52 -0600

    Call api/hello from loading page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect } from "react";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    fetch("api/hello")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
